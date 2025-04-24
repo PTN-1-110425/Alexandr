730082160c112c274ef3436a16ddab56cf8d7050
Alexandr <a.ptrv@yandex.ru>
2025-04-24 10:02:36 +0500

    preparing for first push

---
+++ lesson01/main.py
@@ -0,0 +1,50 @@
+#Функции ввода и вывода
+#Урок первый, домашнее задание №1
+# name = input("Введите имя: ")
+# print("Привет, ", name)
+
+#Урок первый, домашнее задание №2
+# print(3 + 2 + 1)
+#
+# num1 = 3
+# num2 = 2
+# num3 = 1
+# print(num1 + num2 + num3)
+#
+# frst_num = int(input("Введите первое число: "))
+# scnd_num = int(input("Введите второе число: "))
+# thrd_num = int(input("Введите третье число: "))
+# print("Сумма чисел" + ' ' + str(frst_num) + ', ' + str(scnd_num) + ', и ' + str(thrd_num) + ' равна8', frst_num + scnd_num + thrd_num)
+# print(f'Сумма чисел {frst_num}, {scnd_num}, и {thrd_num} равна {frst_num + scnd_num + thrd_num}')
+
+#Урок первый, домашнее задание №3
+# n = int(input("Введите количество школьников: "))
+# k = int(input("Введите количество яблок: "))
+# print("Количество яблок, достанется каждому школьнику:", k // n)
+# print("Количество яблок оставшихся в корзине:", k % n)
+
+#Урок первый, домашнее задание №4
+# nmb = int(input("Введите число: "))
+# print(f'Следующее число для {nmb} --> {nmb + 1}')
+# print(f'Предыдущее число для {nmb} --> {nmb - 1}')
+
+#Урок первый, домашнее задание №5
+# print('''Twinkle, twinkle, little star,
+#     How I wonder what you are!
+#             Up above the world so high,
+#             Like a diamond in the sky.
+# Twinkle, twinkle, little star,
+#         How I wonder what you are''')
+
+#Урок первый, домашнее задание №6
+# totalMoney = 1000
+# quantity = 3
+# price = 450
+# print(f'У меня есть {totalMoney} долларов, поэтому я могу купить {quantity} футбольных меча за {price} долларов.')
+
+a = 'I love Python'
+b = a.title()
+c = a.capitalize()
+print(a[:-1:])
+print(b)
+print(c)
---
+++ lesson02/main.py
@@ -0,0 +1,80 @@
+## Урок второй, домашнее задание №1
+# x = 3
+# y = 5
+# z = 7
+# print(x, y, z)
+# x = x + y + z
+# z = x - y - z
+# y = x - y - z
+# x = x - y - z
+# print(x, y, z)
+
+# tmp = x
+# x = y
+# y = z
+# z = tmp
+# print(x, y, z)
+
+# tmp1 = x
+# tmp2 = y
+# tmp3 = z
+# x = tmp2
+# y = tmp3
+# z = tmp1
+# #print(x, y, z)
+
+# x, y, z = y, z, x
+# print(x, y, z)
+
+## Урок второй, домашнее задание №2
+# num1 = 3.14
+# num2 = 4
+# print(f'{num1} + {num2} = {num1 + num2}')
+# print(f'{num1} - {num2} = {num1 - num2}')
+# print(f'{num1} * {num2} = {num1 * num2}')
+# print(f'{num1} / {num2} = {num1 / num2}')
+# print(f'{num1} ** {num2} = {num1 ** num2}')
+
+## Урок второй, домашнее задание №3
+# str1 = ' << pYthOn - '
+# str2 = ' pYthOn \n . '
+# str3 = ' (( pYthOn - :+ '
+# str1 = str1.lstrip(' << ').rstrip(' - ').lower()
+# str2 = str2.strip().rstrip('. /n ').upper()
+# str3 = str3.strip().lstrip('(( ').rstrip('+: - ').title()
+# print(str1)
+# print(str2)
+# print(str3)
+
+## Урок второй, домашнее задание №4
+# string1 = 'I love python'
+# string2 = 'Hello my dear friend'
+# string3 = 'полиморфизм'
+#
+# string1 = string1[::-1]
+# string2 = string2[6:9] + string2[14:]
+# string3 = string3[::2]
+#
+# print(string1)
+# print(string2)
+# print(string3)
+
+## Урок второй, домашнее задание №5
+# show = 'show ip interface brief'
+# display = show.replace('show', 'display')
+# print(display)
+
+## Урок второй, домашнее задание №6
+# nm = int(input('Введите номер вашего билета: '))
+#
+# if nm % 4 == 0 :
+#     kupe = nm // 4
+# else:
+#     kupe = nm // 4 + 1
+#
+# print(f'Ваше место под номером {nm} находится в {kupe} кабинке')
+
+## Урок второй, домашнее задание №7
+# num1 = input('Введите вше целое число:')
+# print(f'В числе {num1} {len(num1)} цифр(а)(ы).')
+
